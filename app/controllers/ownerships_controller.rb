38effc1b227b731e6617aa2fe3312ba56c4f6c83
Andrew <apepperanderson@gmail.com>
2018-02-03 11:43:45 -0500

    react

---
+++ app/controllers/ownerships_controller.rb
@@ -0,0 +1,4 @@
+class OwnershipsController < ApplicationController
+  def index
+  end
+end
---
+++ app/javascript/containers/OwnershipsIndexContainer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class OwnershipsIndexContainer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return(
+      <div>Hello from the OwnershipsIndexContainer</div>
+    )
+  }
+}
+
+export default OwnershipsIndexContainer
