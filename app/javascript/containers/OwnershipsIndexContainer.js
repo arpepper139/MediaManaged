ac2b1d582892615798e67369dbe87020cab5c451
Andrew <apepperanderson@gmail.com>
2018-02-04 11:22:21 -0500

    more styling

--- app/javascript/containers/OwnershipsIndexContainer.js
+++ app/javascript/containers/OwnershipsIndexContainer.js
@@ -17,14 +17,17 @@ class OwnershipsIndexContainer extends Component {
           <div className="preview">
             <h1 className="preview-header">Movies</h1>
             <div className="row">
-              <div className="col small-12 medium-4 columns">Movie Tile 1</div>
-              <div className="col small-12 medium-4 columns">Movie Tile 2</div>
-              <div className="col small-12 medium-4 columns">Movie Tile 3</div>
+              <div className="col small-12 medium-4 columns"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMTg2MzI1MTg3OF5BMl5BanBnXkFtZTgwNTU3NDA2MTI@._V1_SX300.jpg"></img></div>
+              <div className="col small-12 medium-4 columns"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMTg2MzI1MTg3OF5BMl5BanBnXkFtZTgwNTU3NDA2MTI@._V1_SX300.jpg"></img></div>
+              <div className="col small-12 medium-4 columns"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMTg2MzI1MTg3OF5BMl5BanBnXkFtZTgwNTU3NDA2MTI@._V1_SX300.jpg"></img></div>
             </div>
             <div className="row">
-              <div className="col small-12 medium-4 columns">Movie Tile 4</div>
-              <div className="col small-12 medium-4 columns">Movie Tile 5</div>
-              <div className="col small-12 medium-4 columns">
+              <div className="col small-12 medium-4 columns"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMjQ1MzcxNjg4N15BMl5BanBnXkFtZTgwNzgwMjY4MzI@._V1_SX300.jpg"></img></div>
+              <div className="col small-12 medium-4 columns"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMjQ1MzcxNjg4N15BMl5BanBnXkFtZTgwNzgwMjY4MzI@._V1_SX300.jpg"></img></div>
+              <div className="col small-12 medium-4 columns"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMjQ1MzcxNjg4N15BMl5BanBnXkFtZTgwNzgwMjY4MzI@._V1_SX300.jpg"></img></div>
+            </div>
+            <div className="row preview-footer">
+              <div className="col small-12 large-4 large-offset-8 columns">
                 <Link to="/movies">
                   <button className="index-button">All Movies</button>
                 </Link>
@@ -37,14 +40,17 @@ class OwnershipsIndexContainer extends Component {
           <div className="preview">
             <h1 className="preview-header">Shows</h1>
             <div className="row">
-              <div className="col small-12 medium-4 columns">Show Tile 1</div>
-              <div className="col small-12 medium-4 columns">Show Tile 2</div>
-              <div className="col small-12 medium-4 columns">Show Tile 3</div>
+              <div className="col small-12 medium-4 columns"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BOTA4MTE3MTQwMF5BMl5BanBnXkFtZTgwNzk4MTg4MTI@._V1_SX300.jpg"></img></div>
+              <div className="col small-12 medium-4 columns"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BOTA4MTE3MTQwMF5BMl5BanBnXkFtZTgwNzk4MTg4MTI@._V1_SX300.jpg"></img></div>
+              <div className="col small-12 medium-4 columns"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BOTA4MTE3MTQwMF5BMl5BanBnXkFtZTgwNzk4MTg4MTI@._V1_SX300.jpg"></img></div>
             </div>
             <div className="row">
-              <div className="col small-12 medium-4 columns">Show Tile 4</div>
-              <div className="col small-12 medium-4 columns">Show Tile 5</div>
-              <div className="col small-12 medium-4 columns">
+              <div className="col small-12 medium-4 columns"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMjE3NTQ1NDg1Ml5BMl5BanBnXkFtZTgwNzY2NDA0MjI@._V1_SX300.jpg"></img></div>
+              <div className="col small-12 medium-4 columns"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMjE3NTQ1NDg1Ml5BMl5BanBnXkFtZTgwNzY2NDA0MjI@._V1_SX300.jpg"></img></div>
+              <div className="col small-12 medium-4 columns"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMjE3NTQ1NDg1Ml5BMl5BanBnXkFtZTgwNzY2NDA0MjI@._V1_SX300.jpg"></img></div>
+            </div>
+            <div className="row preview-footer">
+              <div className="col small-12 large-4 large-offset-8 columns">
                 <Link to="/shows">
                   <button className="index-button">All Shows</button>
                 </Link>
