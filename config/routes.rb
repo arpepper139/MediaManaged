11fdcc295324a0c814c0db93094950130fda97a1
Andrew <apepperanderson@gmail.com>
2018-02-05 12:26:35 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'ownerships#index'
-  
+
   get '/home', to: 'ownerships#index'
+  get '/movies/:id', to: 'ownerships#index'
+  get '/shows/:id', to: 'ownerships#index'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
