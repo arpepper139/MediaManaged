73c2f30e270338902b20f872af049dc51a74e4ed
Andrew <apepperanderson@gmail.com>
2018-02-07 11:03:33 -0500

    style tweaks

--- app/javascript/components/MovieShowTile.js
+++ app/javascript/components/MovieShowTile.js
@@ -1,16 +1,25 @@
 import React from 'react'
 
 const MovieShowTile = (props) => {
+  let owned
+  if (props.data.owned) {
+    owned = "Yes"
+  }
+  else {
+    owned = "No"
+  }
+
   return(
     <div className="small-12 large-12 columns">
       <div className="small-12 medium-6 large-4 columns">
         <img className="showpage-poster" src={props.data.poster.url}></img>
       </div>
       <div className="small-12 medium-6 large-8 columns">
-        <p>{props.data.name}</p>
-        <p>{props.data.director}</p>
-        <p>{props.data.year}</p>
-        <p>{props.data.description}</p>
+        <p>Movie: {props.data.name}</p>
+        <p>Director(s): {props.data.director}</p>
+        <p>Year: {props.data.year}</p>
+        <p>Description: {props.data.description}</p>
+        <p>Owned? {owned}</p>
       </div>
     </div>
   )
--- app/javascript/components/ShowShowTile.js
+++ app/javascript/components/ShowShowTile.js
@@ -1,16 +1,25 @@
 import React from 'react'
 
 const ShowShowTile = (props) => {
+  let owned
+  if (props.data.owned) {
+    owned = "Yes"
+  }
+  else {
+    owned = "No"
+  }
+
   return(
     <div className="small-12 large-12 columns">
       <div className="small-12 medium-6 large-4 columns">
         <img className="show-poster" src={props.data.poster.url}></img>
       </div>
       <div className="small-12 medium-6 large-8 columns">
-        <p>{props.data.name}</p>
-        <p>{props.data.writer}</p>
-        <p>{props.data.start_year}-{props.data.end_year}</p>
-        <p>{props.data.description}</p>
+        <p>Show: {props.data.name}</p>
+        <p>Writer(s): {props.data.writer}</p>
+        <p>Years: {props.data.start_year}-{props.data.end_year}</p>
+        <p>Description: {props.data.description}</p>
+        <p>Owned? {owned}</p>
       </div>
     </div>
   )
