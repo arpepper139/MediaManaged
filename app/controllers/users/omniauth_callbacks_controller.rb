7d063964bf6cc40f51dc1bac1a406b0b19109748
Andrew <apepperanderson@gmail.com>
2018-02-18 15:21:22 -0500

    omniauth fix

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,14 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
-    user = User.from_omniauth(request.env["omniauth.auth"])
-    sign_in_and_redirect user
+    facebook_email = request.env["omniauth.auth"]["info"]["email"]
+    email_search = User.where(email: facebook_email)
+
+    if email_search != []
+      user = email_search[0]
+      sign_in_and_redirect user
+    else
+      user = User.from_omniauth(request.env["omniauth.auth"])
+      sign_in_and_redirect user
+    end
   end
 end
