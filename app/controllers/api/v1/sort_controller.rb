a2fe1e1fdf5d2448c71193e98fc6b9336a42b3db
Andrew <apepperanderson@gmail.com>
2018-03-05 22:24:59 -0500

    rating

--- app/controllers/api/v1/sort_controller.rb
+++ app/controllers/api/v1/sort_controller.rb
@@ -36,28 +36,13 @@ class Api::V1::SortController < ApplicationController
     provided_rating = params[:return].to_i
     valid_ratings = [1,2,3,4,5]
 
-    if user != nil && valid_ratings.include?(provided_rating)
-      ownerships = user.movie_ownerships + user.show_ownerships
-      relevant_ownerships = ownerships.select { |ownership| ownership.user_rating == provided_rating }
-
-      if relevant_ownerships == []
-        render json: { results: "Nothing Found" }
-      else
-        relevant_media = []
-        relevant_ownerships.each do |ownership|
-          if ownership.class.name == "MovieOwnership"
-            relevant_media << ownership.movie
-          else
-            relevant_media << ownership.show
-          end
-        end
-
-        render json: { results: relevant_media}
-      end
-    elsif user != nil
-      render json: { error: "Please provide a valid sort parameter" }, status: :unprocessable_entity
-    else
+    if user == nil
       render json: { error: "Not logged in" }, status: 401
+    elsif valid_ratings.include?(provided_rating)
+      relevant_media = sort_by_rating(user, provided_rating)
+      render json: relevant_media
+    else
+      render json: { error: "Please provide a valid sort parameter" }, status: :unprocessable_entity
     end
   end
 
@@ -92,8 +77,26 @@ class Api::V1::SortController < ApplicationController
       results
     end
 
-    def sort_by_rating
+    def sort_by_rating(user, rating)
+      ownerships = user.movie_ownerships + user.show_ownerships
+      relevant_ownerships = ownerships.select { |ownership| ownership.user_rating == rating }
 
+      if relevant_ownerships == []
+        results = { results: "Nothing Found" }
+      else
+        relevant_media = []
+
+        relevant_ownerships.each do |ownership|
+          if ownership.class.name == "MovieOwnership"
+            relevant_media << ownership.movie
+          else
+            relevant_media << ownership.show
+          end
+        end
+
+        results = { results: relevant_media }
+      end
+      results
     end
 
 end
