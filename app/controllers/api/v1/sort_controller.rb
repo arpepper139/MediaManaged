4303ea940fd1b241065f427e636cd22a15dcaeef
Andrew <apepperanderson@gmail.com>
2018-03-05 22:16:05 -0500

    type and genre

--- app/controllers/api/v1/sort_controller.rb
+++ app/controllers/api/v1/sort_controller.rb
@@ -4,53 +4,30 @@ class Api::V1::SortController < ApplicationController
 
   def type
     user = current_user
+    provided_type = params[:return]
 
     if user == nil
       render json: { error: "Not logged in" }, status: 401
-    elsif params[:return] == "movie"
-      if user.movies == []
-        render json: { results: "Nothing Found" }
-      else
-        render json: { results: user.movies }
-      end
-    elsif params[:return] == "show"
-      if user.shows == []
-        render json: { results: "Nothing Found" }
-      else
-        render json: { results: user.shows }
-      end
-    else
+    elsif provided_type != "movie" && provided_type != "show"
       render json: { error: "Please provide a valid sort parameter" }, status: :unprocessable_entity
+    else
+      relevant_media = sort_by_type(user, provided_type)
+      render json: relevant_media
     end
   end
 
   def genre
     user = current_user
+    provided_genre = params[:return]
     valid_genres = Genre.all.map { |genre| genre.name }
 
-    if user != nil
-      if valid_genres.include?(params[:return])
-        sort_genre = params[:return]
-        media = current_user.movies + current_user.shows
-
-        returned_media = []
-        media.each do |media_object|
-          matches = media_object.genres.select { |assigned_genre| assigned_genre.name == sort_genre }
-          if matches != []
-            returned_media << media_object
-          end
-        end
-
-        if returned_media == []
-          render json: { results: "Nothing Found" }
-        else
-          render json: { results: returned_media }
-        end
-      else
-        render json: { error: "Please provide a valid sort parameter" }, status: :unprocessable_entity
-      end
-    else
+    if user == nil
       render json: { error: "Not logged in" }, status: 401
+    elsif valid_genres.include?(provided_genre)
+      relevant_media = sort_by_genre(user, provided_genre)
+      render json: relevant_media
+    else
+      render json: { error: "Please provide a valid sort parameter" }, status: :unprocessable_entity
     end
   end
 
@@ -77,10 +54,46 @@ class Api::V1::SortController < ApplicationController
 
         render json: { results: relevant_media}
       end
-    elsif user!= nil
+    elsif user != nil
       render json: { error: "Please provide a valid sort parameter" }, status: :unprocessable_entity
     else
       render json: { error: "Not logged in" }, status: 401
     end
   end
+
+  private
+
+    def sort_by_type(user, type)
+      plural_type = type + "s"
+      if user.send(plural_type) == []
+        results = { results: "Nothing Found" }
+      else
+        results = { results: user.send(plural_type) }
+      end
+      results
+    end
+
+    def sort_by_genre(user, genre)
+      all_media = user.movies + user.shows
+      returned_media = []
+
+      all_media.each do |media_object|
+        matches = media_object.genres.select { |assigned_genre| assigned_genre.name == genre }
+        if matches != []
+          returned_media << media_object
+        end
+      end
+
+      if returned_media == []
+        results = { results: "Nothing Found" }
+      else
+        results = { results: returned_media }
+      end
+      results
+    end
+
+    def sort_by_rating
+
+    end
+
 end
